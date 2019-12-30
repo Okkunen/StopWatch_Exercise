31c95d00c7034c25f62cfb171e185c166efd7717
Okkunen <tsemi@hotmail.com>
2019-12-30 22:46:18 +0200

    Initial commit

---
+++ StopWatch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch clock = new StopWatch();
+
+            clock.Start();
+            clock.Start();
+            System.Threading.Thread.Sleep(5000);
+            clock.Stop();
+            clock.Stop();
+            clock.Start();
+            System.Threading.Thread.Sleep(3000);
+            clock.Stop();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private bool _isRunning = false;
+
+        public StopWatch()
+        {
+
+        }
+
+        public void Start()
+        {
+            if (_isRunning == false)
+            {
+                _startTime = DateTime.UtcNow;
+                _isRunning = true;
+                Console.WriteLine("Stopwatch was started at {0}", _startTime);
+            }
+            else
+            {
+                Console.WriteLine("Stopwatch is already running.");
+            }
+            
+        }
+
+        public void Stop()
+        {
+            if (_isRunning == true)
+            {
+                _stopTime = DateTime.UtcNow;
+                _isRunning = false;
+                TimeSpan runTime = _stopTime - _startTime;
+                Console.WriteLine("Stopwatch has been stopped. Stopwatch ran for {0}", runTime);
+            }
+            else
+            {
+                Console.WriteLine("Stopwatch is not running");
+            }
+        }
+
+    }
+}
